4f77b0091dc541159e224b66108b5f4f792945cd
Eric Cranston <cracell@gmail.com>
2011-01-10 12:02:55 -0800

    Reverted URL handling. Encode is breaking it now?!

--- lib/s3bear.rb
+++ lib/s3bear.rb
@@ -85,10 +85,11 @@ module S3Bear
     end
 
     def do_download_remote_image
-      io = open(URI.parse(URI.encode(self.send(self.class.url_path))))
+      io = open(URI.parse(self.send(self.class.url_path)))
       def io.original_filename; base_uri.path.split('/').last; end
       io.original_filename.blank? ? nil : io
-    rescue # catch url errors with validations instead of exceptions (Errno::ENOENT, OpenURI::HTTPError, etc...)
+    rescue #OpenURI::HTTPError => error # catch url errors with validations instead of exceptions (Errno::ENOENT, OpenURI::HTTPError, etc...)
+      #puts error
     end
     
     private :image_url_provided?, :download_remote_image, :do_download_remote_image
